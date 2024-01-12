f0f702b9d873c258500468afc9fc02f2cbb3a887
AyanMagerramli <136072693+AyanMagerramli@users.noreply.github.com>
2024-01-12 14:52:50 +0400

    Some Architectural Folders created

---
+++ FallAppAtlProject/Scenes/Login screen/LoginViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewModel.swift
+//  FallAppAtlProject
+//
+//  Created by Ayan on 12.01.24.
+//
+
+import Foundation
---
+++ FallAppAtlProject/Scenes/Register Screen/RegisterViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterViewModel.swift
+//  FallAppAtlProject
+//
+//  Created by Ayan on 12.01.24.
+//
+
+import Foundation
